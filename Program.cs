17363d32d78c3a5aeb55de5475a7b1e115b222b6
Kol <62248045+Kol-Dev@users.noreply.github.com>
2022-06-01 17:05:16 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace RemoveDiacritics
             var newText = text.RemoveDiacritics();
             // for chars
             "Ã"[0].RemoveDiacritics(); // A
-            var charsToRemove = new string[] { "@", ",", "?", ".", ";", "!", "&", "'", "#", "(", ")", "{", "}", "[", "]", "-", "_", "$", "*", "=", "+", "/", ":" };
+            var charsToRemove = new string[] { "@", ",", "?", "§", ".", ";", "!", "&", "'", "#", "(", ")", "{", "}", "[", "]", "-", "_", "$", "*", "=", "+", "/", ":" };
             foreach (var c in charsToRemove)
             {
                 newText = newText.Replace(c, string.Empty);
@@ -29,7 +29,7 @@ namespace RemoveDiacritics
             Console.ResetColor();
             string textInput = Console.ReadLine();
             var newTextInput = textInput.RemoveDiacritics();
-            var removeChars = new string[] { "@", ",", "?", ".", ";", "!", "&", "'", "#", "(", ")", "{", "}", "[", "]", "-", "_", "$", "*", "=", "+", "/", ":" };
+            var removeChars = new string[] { "@", ",", "?", "§", ".", ";", "!", "&", "'", "#", "(", ")", "{", "}", "[", "]", "-", "_", "$", "*", "=", "+", "/", ":" };
             int value = 0;
             foreach (var c in removeChars)
             {
@@ -37,8 +37,57 @@ namespace RemoveDiacritics
             }
             foreach(char c in newTextInput)
             {
-                int index = (int)c % 32;
-                value += index;
+                switch(c)
+                {
+                    case '1':
+                        Console.WriteLine(c + " +" + c);
+                        value += 1;
+                        break;
+                    case '2':
+                        Console.WriteLine(c + " +" + c);
+                        value += 2;
+                        break;
+                    case '3':
+                        Console.WriteLine(c + " +" + c);
+                        value += 3;
+                        break;
+                    case '4':
+                        Console.WriteLine(c + " +" + c);
+                        value += 4;
+                        break;
+                    case '5':
+                        Console.WriteLine(c + " +" + c);
+                        value += 5;
+                        break;
+                    case '6':
+                        Console.WriteLine(c + " +" + c);
+                        value += 6;
+                        break;
+                    case '7':
+                        Console.WriteLine(c + " +" + c);
+                        value += 7;
+                        break;
+                    case '8':
+                        Console.WriteLine(c + " +" + c);
+                        value += 8;
+                        break;
+                    case '9':
+                        Console.WriteLine(c + " +" + c);
+                        value += 9;
+                        break;
+                    case '0':
+                        Console.WriteLine(c + " +10");
+                        value += 10;
+                        break;
+                    case '"':
+                        break;
+                    default:
+                        int index = (int)c % 32;
+                        Console.WriteLine(c + " +" + index);
+                        value += index;
+                        break;
+                }       
+                
             }
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Result:");
